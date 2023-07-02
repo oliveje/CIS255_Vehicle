c3bacc529658935d82c808e5c4b8f16d38591126
oliveje <135443210+oliveje@users.noreply.github.com>
2023-07-02 08:28:45 -0500

    Add files via upload
    Adding initial Java file to Repo

---
+++ src/CIS255_Vehicle.java
@@ -0,0 +1,10 @@
+public class CIS255_Vehicle {
+  public static void main(String[] args) {
+    System.out.println("Vehicle Type?");
+  }
+  
+  //public class Make extends Vehicle {}
+  
+  //public class Type extends make {}
+  
+}
